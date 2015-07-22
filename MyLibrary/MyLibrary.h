fd61e3fde63f59b2ce413322c2ea199af23df9b6
miyamoto <miyamoto.kouichi@meta.co.jp>
2015-07-22 13:49:54 +0900

    first commit

---
+++ MyLibrary/MyLibrary.h
@@ -0,0 +1,13 @@
+//
+//  MyLibrary.h
+//  MyLibrary
+//
+//  Created by miyamoto on 2015/07/22.
+//  Copyright (c) 2015年 metatechno. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyLibrary : NSObject
+
+@end
---
+++ MyLibrary/MyLibrary.m
@@ -0,0 +1,13 @@
+//
+//  MyLibrary.m
+//  MyLibrary
+//
+//  Created by miyamoto on 2015/07/22.
+//  Copyright (c) 2015年 metatechno. All rights reserved.
+//
+
+#import "MyLibrary.h"
+
+@implementation MyLibrary
+
+@end
